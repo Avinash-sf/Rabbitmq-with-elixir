b2a041ebc3857024ea1f8612108c6ceef82638dd
Avinash <avinash.vadlamudi@gmail.com>
2023-03-13 10:57:44 +0530

    updated `amqp` to 3.2.0 from `hex`

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule RabbitmqWithElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:amqp, "~> 2.0.0-rc.1"}
+      {:amqp, "~> 3.2"}
     ]
   end
 end
